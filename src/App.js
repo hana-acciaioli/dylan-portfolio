82424dde86b28871d0768925d36c74727b5059ce
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-03-13 20:35:45 -0700

    initial commit with base components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth/Auth';
+import Clients from './components/Clients/Clients.js';
+import Contact from './components/Contact/Contact.js';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/clients" component={Clients} />
+        <Route path="/contact" component={Contact} />
         <Route path="/auth/:type" component={Auth} />
-        <Route path="*">
-          <Redirect to="/auth/sign-in" />
-        </Route>
       </Switch>
     </div>
   );
