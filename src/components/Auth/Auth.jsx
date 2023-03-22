2a6e17d77b7e4e055cf7a0f9ac63b6b3479ad016
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-03-22 13:00:12 -0700

    installed cloudinary, bootstrap, and axios. putting image upload and fetch logic on hold for now to work on data logic

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -12,7 +12,7 @@ export default function Auth() {
   const { user, setUser } = useUser();
 
   if (user) {
-    return <Redirect to="/whatever" />;
+    return <Redirect to="/" />;
   }
 
   const submitAuth = async () => {
